52c3edd2896c1d9a0b632caaec6055c92abb9477
diepnguyen92 <148781455+diepnguyen92@users.noreply.github.com>
2024-05-01 21:09:59 +0700

    up

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./component/Header";
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <section className="todoapp">
+        <Header />
+     
+       
+      </section>
+    </>
+  );
+};
+
+export default App;
